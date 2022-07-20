787a1deebfc91fac4d6a1e7e57588ccc3be0028f
Alejandro Moñux Bernal <43568325+alejandromonux@users.noreply.github.com>
2022-07-20 12:50:16 +0200

    Fix: Adapted XGBoost to constructData changes

--- Classification/Boosting/xgboost.py
+++ Classification/Boosting/xgboost.py
@@ -7,8 +7,8 @@ from Classification.CommonFunctions import trainingAndTest
 from Classification.DecisionTree.TreeLogic import constructData
 import xgboost as xgb
 
-def XGBoost():
-    data = constructData(os.getcwd() + "\database.json", onlytitles=True)
+def  XGBoost():
+    data,titles = constructData(os.getcwd() + "/adjusted_database.json", onlytitles=False, merge=False)
     train_x, test_x, train_y, test_y = train_test_split(data["x"], data["y"],
                                                                         random_state=420,
                                                                         test_size=0.3)
