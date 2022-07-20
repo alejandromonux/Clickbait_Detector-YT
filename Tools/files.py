46c2f7f782656ef2ffcf2f3ef8923c85790aaa74
Alejandro Moñux Bernal <43568325+alejandromonux@users.noreply.github.com>
2022-07-20 12:48:04 +0200

    Feat: Updated files.py to read CSV

--- Tools/files.py
+++ Tools/files.py
@@ -1,5 +1,49 @@
+import csv
 import json
+import numpy as np
 
+def readCSVManual(name):
+    x = []
+    y = []
+    file = open(name, encoding="utf8")
+    line=file.readline()
+    #Skippeamos la primera, que es la cabecera
+    line = file.readline()
+    while line != "":
+        x.append(line.split(",")[0])
+        y.append(int(line.split(",")[1]))
+        line=file.readline()
+    return x, y
+
+def readCSV(name):
+    x = []
+    y = []
+    with open(name, encoding='utf-8') as file:
+        csvFile = csv.DictReader(file)
+        for line in csvFile:
+            x.append(line["headline"])
+            y.append(line["clickbait"])
+    return x,y
+
+def readCSVKaggle(name,rating):
+    x = []
+    y = []
+    with open(name, encoding='utf-8') as file:
+        csvFile = csv.DictReader(file)
+        for line in csvFile:
+            x.append({"author":"",
+                      "subscribers":"0",
+                      "title": line["title"],
+                      "description": "",
+                      "category": "0",
+                      "publishDate": "",
+                      "views": line["total_views"],
+                      "likes": "0",
+                      "fav_count": "0",
+                      "num_comments": "0",
+                      "id":line["video_id"]})
+            y.append(rating)
+    return x,y
 
 def readFile(name):
     file = open(name, encoding="utf8")
