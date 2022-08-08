87e600ef768fe4e9fd9d0ac8f8b9a3a995425829
Alejandro Moñux Bernal <43568325+alejandromonux@users.noreply.github.com>
2022-08-08 17:18:16 +0200

    Fix: attempt at fixing railways crash

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,8 @@ from web_config import Config
 """---VARIABLES---"""
 userCounter = 0
 cache = {}
-file_route=os.getcwd() + "\\DataRetrieval\\adjusted_database.json"
-model = Model(readFile(os.getcwd() + "\\DataRetrieval\\adjusted_database.json"), [[],[]],willImport=True)
+file_route="\\DataRetrieval\\adjusted_database.json"
+model = Model(readFile("\\DataRetrieval\\adjusted_database.json"), [[],[]],willImport=True)
 model.loadModel(prefix="\\DataRetrieval\\")
 
 """---FLASK PROCESSES---"""
