3dda484bbf99081788d2c827516fef489447724f
Alejandro Moñux Bernal <43568325+alejandromonux@users.noreply.github.com>
2022-08-13 18:36:24 +0200

    Feat: More logging methods

--- Tools/logs.py
+++ Tools/logs.py
@@ -9,3 +9,10 @@ def appendToLogs(filename,object, rating):
             return 0
     db["list"].append(object)
     writeFile(filename,db)
+def modifyLogs(filename, object, opinion):
+    db=readFile(filename)
+    for i  in range(len(db["list"])):
+        if db["list"][i]["title"]==object["title"]:
+            db["list"][i]["opinion"] = opinion
+            break
+    writeFile(filename,db)
